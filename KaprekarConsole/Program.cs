02036989820e0b1e064f22871b1174307a5de236
Jeff Blanchard <jeffrey.blanchard@ais.com>
2025-01-18 16:50:51 -0500

    Add project files.

---
+++ KaprekarConsole/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace KaprekarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get user input and validate
+            string? inputAsString = null;
+            do
+            {
+                Console.WriteLine("Welcome to the Kaprekar's Calculator. \n Please enter 4 unique numbers");
+                inputAsString = Console.ReadLine();
+
+
+            } while (!IsValidInput(inputAsString));
+
+            runKoprekarsAlgorithm(inputAsString);
+
+
+
+
+        }
+        public static bool IsValidInput(string inputAsString)
+        {
+            if (inputAsString == null || inputAsString.Length != 4 || !int.TryParse(inputAsString, out _))
+            {
+                Console.WriteLine("Invalid input. Please enter exactly 4 unique digits.");
+                return false;
+            }
+            else if (inputAsString.Length != inputAsString.Distinct().Count())
+            {
+                Console.WriteLine("Invalid input. Please enter 4 unique digits.");
+                return false;
+            }
+
+            foreach (char num in inputAsString)
+            {
+                if (num == '0')
+                {
+                    Console.WriteLine("Invalid input. Please enter 4 unique digits without any zeros.");
+                    return false;
+                }
+            }
+ 
+            return true;
+        }
+
+        public static void runKoprekarsAlgorithm(string inputAsString)
+        {
+            int rounds = 0;
+            int sortedAscending = int.Parse(new string(inputAsString.OrderBy(c => c).ToArray()));
+            int sortedDescending = int.Parse(new string(inputAsString.OrderByDescending(c => c).ToArray()));
+
+            int total = sortedDescending - sortedAscending;
+            do
+            {
+                string strTotal = total.ToString();
+                sortedAscending = int.Parse(new string(strTotal.OrderBy(c => c).ToArray()));
+                sortedDescending = int.Parse(new string(strTotal.OrderByDescending(c => c).ToArray()));
+
+                total = sortedDescending - sortedAscending;
+
+
+                rounds++;
+                Console.WriteLine($"Round {rounds}: {sortedDescending} - {sortedAscending} = {total}");
+
+            } while (total != 6174);
+
+        }
+    }
+}
---
+++ KaprekarConsole/README.md
@@ -0,0 +1,38 @@
+﻿# Kaprekar's Routine Console Application
+
+https://www.youtube.com/watch?v=d8TRcZklX_Q
+
+1. Choose any four unique numbers. You cannot repeat a number. For example 1874.
+
+1. Take the number chosen and put it in ascending order
+8741
+
+2. Take the number chosen and put it in descending order
+1478
+
+3. Subtract the numbers ascending from descending
+
+```math
+8741 - 1748 = 6993
+```
+
+1. repeat the steps 2 through 4 again with the new number. IE 6993
+
+```math
+9963 - 3699 = 5934
+```
+
+Keep repeating the sequence and you will eventually get to 6174. 
+
+psuedo code
+
+1. get the 4 digit number from the console. 
+2. test if the input is valid, if not, provide a friendly error and ask again
+    1. is the string null? 
+    2. can the string (input) be converted to a number (no alpha chars)
+    3. is the string the right length (4)
+    4. are there any zeros in the provided number?
+3. take each character for the string and then sort it ascending
+4. take each character for the string and then sort it descending
+5. Subtract the descending from the ascending number. If the result is not 6174, repeat steps 3-5 with the new total until you get to 6174
+4. Print each rounds result to the console
